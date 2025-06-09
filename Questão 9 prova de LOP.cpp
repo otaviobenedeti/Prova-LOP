59fcc4971465269b6b0357a9a2bfa365bf870c5a
otaviobenedeti <otaviombenedeti@gmail.com>
2025-06-09 10:10:58 -0300

    Add files via upload

---
+++ Questão 9 prova de LOP.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct aluno {
+    char nome[50];
+    int idade;
+    float nota;
+};
+
+aluno alunos[3];
+
+void listaralunos() {
+    for (int i = 0; i < 3; i++) {
+        printf("\nAluno %d:\n", i + 1);
+        printf("Nome: %s\n", alunos[i].nome);
+        printf("Idade: %d\n", alunos[i].idade);
+        printf("Nota: %.2f\n", alunos[i].nota);
+    }
+}
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+        printf("\nDigite os dados do aluno %d:\n", i + 1);
+        printf("Nome:\n");
+        scanf("%s", alunos[i].nome); 
+        printf("Idade:\n");
+        scanf("%d", &alunos[i].idade);
+        printf("Nota:\n");
+        scanf("%f", &alunos[i].nota);
+    }
+
+    listaralunos();
+
+    return 0;
+}
